d6206483db0ec314fb48b4d5785240007aae9f2e
MohamedAliBadawy <medooali6262@gmail.com>
2025-10-02 00:33:21 +0300

    v4.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1366465279"
+    serviceWorkerVersion: "3631140677"
   }
 });
